abde06b20e64f7ea8b4c1ccb3b44d69fef80e2a2
Rubenslourenco <alvesrubens926@gmail.com>
2025-02-14 09:16:00 -0300

    Add useLocation no mostrar tudo de artists e songs

---
+++ .history/src/components/ItemList_20250214090122.jsx
@@ -0,0 +1,34 @@
+import { Link, useLocation } from "react-router-dom";
+import SingleItem from "./SingleItem"; //
+
+// eslint-disable-next-line react/prop-types
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathName } = useLocation();
+  return (
+    <div>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>{title}</h2>
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link>
+        </div>
+
+        <div className="item-list__container">
+          {itemsArray
+            // eslint-disable-next-line react/prop-types
+            .filter((currentvalue, index) => index < items)
+            .map((currObj, index) => (
+              <SingleItem
+                idPath={idPath}
+                {...currObj}
+                key={`${title}-${index}`}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
